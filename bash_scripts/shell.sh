96b094ad5bbd4068cfb4c812776813b12ae6a5e3
Silletr <hristicenkoigor764@gmail.com>
2025-07-05 16:37:38 +0000

    modified:   bash_scripts/shell.sh

--- bash_scripts/shell.sh
+++ bash_scripts/shell.sh
@@ -5,13 +5,17 @@ while true; do
   read -rp "LazyDevOS> " cmd args
   case $cmd in
     help)
-      echo "Команды: help, echo, clear, exit";;
+      echo "Команды: help, echo, clear, exit, ping, hack";;
     echo)
       echo $args;;
     clear)
       clear;;
     exit)
       echo "Shutting down..."; break;;
+    ping)
+      echo "Pong";;
+    hack)
+      echo "Succefully hacked.. but what? ";;
     *)
       echo "Unknown command: $cmd";;
   esac
